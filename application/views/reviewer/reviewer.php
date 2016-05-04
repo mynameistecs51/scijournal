e821b12f80dddc0692d20c67b5c9675ddbb11b5d
Chaiwat <mynameistecs51@gmail.com>
2016-05-04 07:58:10 +0700

    update reviewer

--- application/views/reviewer/reviewer.php
+++ application/views/reviewer/reviewer.php
@@ -1,8 +1,35 @@
 <?php echo $header;?>
 <?php $this->load->view('reviewer/head_menu');?>
-<?php
-	foreach ($read_journal as $read) {
-		echo "journal title:",$read['j_title'],"<br/>";
-	}
- ?>
+<div class="panel-body">
+	<div class="row">
+		<div class="col-lg-12">
+			<div class="table-responsive">
+				<!-- <table class="table table-bordered table-hover table-striped"> -->
+				<table id ="" class="display" cellspacing="0" width="100%">
+					<thead>
+						<tr>
+							<th class="col-sm-1">#</th>
+							<th class="text-center">DETAIL</th>
+							<!-- <th class="col-sm-2">date send</th> -->
+							<th class="col-sm-4 text-center">STATUS</th>
+						</tr>
+					</thead>
+					<tbody>
+						<?php	
+						$num = count($read_journal);
+						foreach ($read_journal as $read) : 
+							?>
+						<tr>
+							<td><?php echo $num-- ; ?></td>
+							<td><?php echo $read['j_title']; ?></td>
+							<td>test</td>
+						</tr>
+					<?php endforeach;?>
+				</tbody>
+			</table>
+		</div>
+	</div>
+</div><!-- /.end row  -->
+</div> <!-- /. end panel // -->
+
 <?php echo $footer;?>
